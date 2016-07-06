2eeadacd13ea597180a2ede9cf9cfcc720d7911b
JoeChapman <joseph.philip.chapman@gmail.com>
2016-07-06 16:26:43 +0100

    Remove configuration from bootstrap function, it could be misleading

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const bootstrap = require('hof-bootstrap');
 // See https://github.com/UKHomeOffice/hof-bootstrap for details
 
 bootstrap({
-  views: false,
-  fields: false,
   routes: [{
-    fields: 'any',
-    steps: {
-      '/one': {
-      }
-    }
+    steps: {}
   }]
 });
