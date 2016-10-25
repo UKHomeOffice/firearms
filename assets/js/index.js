a60ffe461e06865f88de285bf3fb1ef3e279ae7b
Ben Diggle <ben.diggle@transformuk.com>
2016-10-25 15:01:47 +0100

    Call the detailsSummary function in the toolkit. Fixes and issue with Microsoft browsers with details summary section not being clickable and not toggling the summary

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ var toolkit = require('hof').toolkit;
 var helpers = toolkit.helpers;
 var progressiveReveal = toolkit.progressiveReveal;
 var formFocus = toolkit.formFocus;
+toolkit.detailsSummary();
 
 helpers.documentReady(progressiveReveal);
 helpers.documentReady(formFocus);
