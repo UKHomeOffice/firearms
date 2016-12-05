6b4f703ddd5366dfeb926e2a2016fcf5ee922def
Ben Diggle <ben.diggle@transformuk.com>
2016-12-05 13:59:52 +0000

    Fix indentation on the acceptance tests

--- apps/shooting-clubs/acceptance/features/activity.js
+++ apps/shooting-clubs/acceptance/features/activity.js
@@ -7,15 +7,15 @@ Feature('Activity step');
 Before((
   I,
   activityPage
-  ) => {
-    I.visitPage(activityPage, steps);
+) => {
+  I.visitPage(activityPage, steps);
 });
 
 Scenario('When I submit the page, I am taken to the club name step', (
   I,
   activityPage,
   clubNamePage
-  ) => {
+) => {
   I.click(activityPage.new);
   I.submitForm();
   I.seeInCurrentUrl(clubNamePage.url);
--- apps/shooting-clubs/acceptance/features/club-name.js
+++ apps/shooting-clubs/acceptance/features/club-name.js
@@ -13,28 +13,25 @@ Before((
 
 Scenario('The correct form element appears', (
   I,
-    clubNamePage
-  ) => {
-    I.seeElement(clubNamePage.clubName);
-  }
-);
+  clubNamePage
+) => {
+  I.seeElement(clubNamePage.clubName);
+});
 
 Scenario('I see an error when I submit an empty form', (
   I,
-    clubNamePage
-  ) => {
-    I.submitForm();
-    I.seeErrors(clubNamePage.clubName);
-  }
-);
+  clubNamePage
+) => {
+  I.submitForm();
+  I.seeErrors(clubNamePage.clubName);
+});
 
 Scenario('When I correctly submit the form I am taken to club-postcode step', (
   I,
-    clubNamePage,
-    clubPostCodePage
-  ) => {
-    I.fillField(clubNamePage.clubName, clubNamePage.name)
-    I.submitForm();
-    I.seeInCurrentUrl(clubPostCodePage.url);
-  }
-);
+  clubNamePage,
+  clubPostCodePage
+) => {
+  I.fillField(clubNamePage.clubName, clubNamePage.name)
+  I.submitForm();
+  I.seeInCurrentUrl(clubPostCodePage.url);
+});
