5108155dc661c7a74b00178e2fe9baef94aa61ee
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-12-08 13:52:25 +0000

    Club Secretary page: * Remove extra indentation * Add EOL on files * Simplify and reduce code

--- apps/shooting-clubs/acceptance/features/club-secretary-name.js
+++ apps/shooting-clubs/acceptance/features/club-secretary-name.js
@@ -5,34 +5,32 @@ const steps = require('../../');
 Feature('Club secretary step');
 
 Before((
-    I,
-    clubSecretaryPage
+  I,
+  clubSecretaryPage
 ) => {
-    I.visitPage(clubSecretaryPage, steps);
+  I.visitPage(clubSecretaryPage, steps);
 });
 
 Scenario('check there is the correct form elements', (
-    I,
-    clubSecretaryPage
+  I,
+  clubSecretaryPage
 ) => {
-    I.seeElement('#club-secretary-name')
+  I.seeElement(clubSecretaryPage['club-secretary-name-id'])
 });
 
 Scenario('an error message appears if I submit an empty input field', (
-    I,
-    clubSecretaryPage
+  I,
+  clubSecretaryPage
 ) => {
-    I.submitForm();
-    I.seeErrors(clubSecretaryPage['club-secretary-name']);
+  I.submitForm();
+  I.seeErrors(clubSecretaryPage['club-secretary-name-id']);
 });
 
 Scenario('I am taken to the appropriate next page', (
-    I,
-    clubSecretaryPage,
-    clubSecretaryAddAddress
+  I,
+  clubSecretaryPage,
+  clubSecretaryAddAddress
 ) => {
-    clubSecretaryPage.fillFormAndSubmit();
-    I.seeInCurrentUrl(clubSecretaryAddAddress.url);
+  clubSecretaryPage.fillFormAndSubmit();
+  I.seeInCurrentUrl(clubSecretaryAddAddress.url);
 });
-
-
--- apps/shooting-clubs/acceptance/pages/club-secretary-name.js
+++ apps/shooting-clubs/acceptance/pages/club-secretary-name.js
@@ -3,19 +3,15 @@
 let I;
 
 module.exports = {
-
     _init(){
       I = require('so-acceptance/steps.js')();
     },
 
     url: 'club-secretary-name',
-    'club-secretary-name': '#club-secretary-name',
-    content: {
-      'name': 'Michael Knight'
-    },
+    'club-secretary-name-id': '#club-secretary-name',
 
     fillFormAndSubmit(){
-        I.fillField(this['club-secretary-name'], this.content['name']);
-        I.submitForm();
+      I.fillField(this['club-secretary-name-id'], 'BA baracus');
+      I.submitForm();
     }
 };
