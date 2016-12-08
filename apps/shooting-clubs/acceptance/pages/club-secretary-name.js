3c0622effc12c961aad4f49c651a849f5fe4bb3f
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-12-08 14:30:53 +0000

    sort out the indenting

--- apps/shooting-clubs/acceptance/pages/club-secretary-name.js
+++ apps/shooting-clubs/acceptance/pages/club-secretary-name.js
@@ -3,15 +3,15 @@
 let I;
 
 module.exports = {
-    _init(){
-      I = require('so-acceptance/steps.js')();
-    },
+  _init(){
+    I = require('so-acceptance/steps.js')();
+  },
 
-    url: 'club-secretary-name',
-    'club-secretary-name-id': '#club-secretary-name',
+  url: 'club-secretary-name',
+  'club-secretary-name-id': '#club-secretary-name',
 
-    fillFormAndSubmit(){
-      I.fillField(this['club-secretary-name-id'], 'BA baracus');
-      I.submitForm();
-    }
+  fillFormAndSubmit(){
+    I.fillField(this['club-secretary-name-id'], 'BA baracus');
+    I.submitForm();
+  }
 };
