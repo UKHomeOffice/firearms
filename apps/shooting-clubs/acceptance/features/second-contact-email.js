450fcf143369a5150975103c496c8a797f1e3c8d
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-12-15 14:34:54 +0000

    Second Contact Email page: (#87)
    * Second Contact Email page:
    * Add the fields to the steps file & fields file
    * Add the acceptance tests
    * Add the translations
    https://jira.digital.homeoffice.gov.uk/browse/FLHO-348
    
    * Second contact email feature:
    - fix the name of the test to go to next step

---
+++ apps/shooting-clubs/acceptance/features/second-contact-email.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const steps = require('../../');
+const _ = require('lodash');
+
+Feature('second contact email step');
+
+Before((
+  I,
+  secondContactEmailPage
+) => {
+  I.visitPage(secondContactEmailPage, steps);
+});
+
+Scenario('check there is the correct form elements', (
+  I,
+  secondContactEmailPage
+) => {
+  I.seeElements(_.values(secondContactEmailPage.fields));
+});
+
+Scenario('check that an error appears if I submit empty fields', (
+  I,
+  secondContactEmailPage
+) => {
+  I.submitForm();
+  I.seeErrors(_.values(secondContactEmailPage.fields));
+});
+
+Scenario('I can go to the next page, location postcode page', (
+  I,
+  secondContactEmailPage,
+  locationPostcodePage
+) => {
+  secondContactEmailPage.fillFormAndSubmit('valid-email');
+  I.seeInCurrentUrl(locationPostcodePage.url);
+});
+
+Scenario('An error is shown if an invalid email address is entered', (
+  I,
+  secondContactEmailPage
+) => {
+  secondContactEmailPage.fillFormAndSubmit('invalid-email');
+  I.seeErrors(secondContactEmailPage.fields['second-contact-email-id']);
+});
