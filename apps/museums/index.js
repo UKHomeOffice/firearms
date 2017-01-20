53d4a40fb7f691fd68b85e7c8f00f71cb0fb9c25
Leonard Martin <leonard.martin@gmail.com>
2017-01-20 15:16:05 +0000

    Initial implementation of museums journey

---
+++ apps/museums/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const controllers = require('hof-controllers');
+
+module.exports = {
+  name: 'museums',
+  baseUrl: '/museums',
+  steps: {
+    '/': {
+      controller: controllers.start,
+      next: '/activity'
+    },
+    '/activity': {
+      fields: ['activity'],
+      next: '/name'
+    },
+    '/name': {
+      fields: ['name'],
+      next: '/address-loop',
+      locals: {
+        section: 'name'
+      }
+    },
+    '/address-loop': {
+      next: '/contact-name'
+    },
+    '/contact-name': {
+      fields: ['contact-name'],
+      next: '/contact-details',
+      locals: {
+        section: 'contact-details'
+      }
+    },
+    '/contact-details': {
+      fields: ['contact-email', 'contact-phone'],
+      next: '/contact-address',
+      locals: {
+        section: 'contact-details'
+      }
+    },
+    '/contact-address': {
+      fields: ['same-contact-address'],
+      next: '/contact-address-select',
+      forks: [{
+        target: '/contact-address-postcode',
+        condition: {
+          field: 'same-contact-address',
+          value: 'no'
+        }
+      }]
+    },
+    '/contact-address-postcode': {
+      next: '/contact-address-select'
+    },
+    '/contact-address-select': {
+      next: '/confirm'
+    },
+    '/confirm': {
+      template: 'confirm',
+      controller: require('./controllers/confirm'),
+      fieldsConfig: require('./fields'),
+      next: '/confirmation'
+    },
+    '/confirmation': {}
+  }
+};
