566b248b40db5c5460bd475198d41cd58293392e
Leonard Martin <leonard.martin@gmail.com>
2017-01-30 13:02:56 +0000

    Initial implementation of museums (#104)
    * Initial implementation of museums journey
    
    * Fix import path for common translations
    
    * add confirmation page
    
    * remove unnecessary specific confirmation.html for museums, use the common one

---
+++ apps/museums/controllers/confirm.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Controller = require('hof-controllers').confirm;
+
+module.exports = class ConfirmController extends Controller {
+  locals(req, res) {
+    const content = req.rawTranslate('pages.confirm');
+    const locals = super.locals(req, res);
+    const rows = locals.rows.filter(row => row.fields === undefined || row.fields.length);
+    return Object.assign({}, locals, {
+      content,
+      rows
+    });
+  }
+};
