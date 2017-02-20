245f76e23072ed8c98e852fd2fb440e4aacf04a1
Leonard Martin <leonard.martin@gmail.com>
2017-02-20 11:27:18 +0000

    Remove fields from locals which are dependently displayed
    Where a field is displayed as a child of another, remove it from the list of fields to render so it does not render twice.

--- apps/common/controllers/base.js
+++ apps/common/controllers/base.js
@@ -3,11 +3,15 @@
 const Controller = require('hof-form-controller');
 
 module.exports = class BaseController extends Controller {
+
   locals(req, res) {
     const locals = super.locals(req, res);
     const renew = req.sessionModel.get('activity') === 'renew' && locals.renew === true;
+    const fields = locals.fields.filter(field => !req.form.options.fields[field.key].dependent);
     return Object.assign({}, locals, {
+      fields,
       renew
     });
   }
+
 };
