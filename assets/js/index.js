e7b0c7233c995ba9ea58f05588d0c62960232f6d
Leonard Martin <leonard.martin@gmail.com>
2017-05-25 15:29:29 +0100

    Use theme assets
    Instead of laoding hof-frontend-toolkit in as a separate dependency

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,6 @@
 'use strict';
 
-var toolkit = require('hof-frontend-toolkit');
-var helpers = toolkit.helpers;
-var progressiveReveal = toolkit.progressiveReveal;
-var formFocus = toolkit.formFocus;
-toolkit.detailsSummary();
-
-helpers.documentReady(progressiveReveal);
-helpers.documentReady(formFocus);
+require('hof-theme-govuk');
 
 var $ = require('jquery');
 var typeahead = require('typeahead-aria');
