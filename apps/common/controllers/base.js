18b02ff2b85445eb4197cace8abe5331547fb179
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-08-04 14:51:21 +0100

    FA-33: Added 'Vary' to the activity page for shooting-clubs, new-dealer and museums.
                    * Added 'Vary' selection to shooting-clubs, new-dealer and museums index.js pages.
                    * Updated fields.json to include new option.
                    * Updated submission.js in shooting-clubs, new-dealer and museums logic for new option.
                    * Updated index.js in shooting-clubs, new-dealer and museums for new fork step which follows the same as 'renew'.
                    * Added additional unit tests for 'vary' in base.test.js
                    * Changed find in submission.js for shooting-clubs, new-dealer and museums

--- apps/common/controllers/base.js
+++ apps/common/controllers/base.js
@@ -5,7 +5,8 @@ const Controller = require('hof').controller;
 module.exports = class BaseController extends Controller {
   locals(req, res) {
     const locals = super.locals(req, res);
-    const renew = req.sessionModel.get('activity') === 'renew' && locals.renew === true;
+    const activity = req.sessionModel.get('activity');
+    const renew = (activity === 'renew' || activity === 'vary') && locals.renew === true;
     const fields = locals.fields.filter(field => !req.form.options.fields[field.key].dependent);
     const usage = req.sessionModel.get('usage') || [];
     return Object.assign({}, locals, {
