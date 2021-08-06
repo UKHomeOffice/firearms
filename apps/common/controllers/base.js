23d324d9246d3fa8837f4bd73fb693c8c98fb146
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-08-06 14:32:08 +0100

    FA-33: Correction in base.js and package.json
    	* Refactored logic in renew const in base.js
    	* Removed Node version in package.json

--- apps/common/controllers/base.js
+++ apps/common/controllers/base.js
@@ -7,7 +7,7 @@ module.exports = class BaseController extends Controller {
   locals(req, res) {
     const locals = super.locals(req, res);
     const activity = req.sessionModel.get('activity');
-    const renew = (activity === 'renew' || activity === 'vary') && locals.renew === true;
+    const renew = (activity === 'renew' || activity === 'vary') && locals.renew;
     const fields = locals.fields.filter(field => !req.form.options.fields[field.key].dependent);
     const usage = req.sessionModel.get('usage') || [];
     return Object.assign({}, locals, {
