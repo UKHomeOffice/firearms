33dcd0479390eb995fa0d5de7597fadf30aff453
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-08-18 12:17:47 +0100

    FA-34: Added upload existing authority functionality to s5 new-dealer
    	* Added upload existing authority to index.js, submission.js and fields.json
    	* Added unit tests for s5 in submission.spec.js

--- apps/new-dealer/index.js
+++ apps/new-dealer/index.js
@@ -53,10 +53,37 @@ module.exports = {
         'reference-number',
         'authority-number'
       ],
-      next: '/company-name',
+      next: '/existing-authority',
       locals: {
         section: 'business-details'
       }
+    },
+    '/existing-authority': {
+      controller: require('../common/controllers/existing-authority-documents'),
+      fields: [
+        'existing-authority-upload',
+        'existing-authority-description'
+      ],
+      continueOnEdit: true,
+      next: '/existing-authority-add-another'
+    },
+    '/existing-authority-add-another': {
+      template: 'existing-authority-documents-add-another',
+      controller: require('../common/controllers/existing-authority-documents-add-another'),
+      behaviours: [require('./common/behaviours/existing-authority-documents-add')],
+      fields: [
+        'existing-authority-add-another'
+      ],
+      forks: [{
+        isLoop: true,
+        target: '/existing-authority',
+        condition: {
+          field: 'existing-authority-add-another',
+          value: 'yes'
+        }
+      }],
+      continueOnEdit: true,
+      next: '/company-name'
     },
     '/company-name': {
       fields: [
