6de8841bbfb68239caeb2756857a388b81d7f1bb
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-12-01 14:32:18 +0000

    filevault changed timeout in clamav and max file size

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const localhost = () => `${process.env.LISTEN_HOST || '0.0.0.0'}:${process.env.P
 module.exports = {
   env: env,
   upload: {
-    maxfilesize: '50mb',
+    maxfilesize: '20mb',
     hostname: (!env || env === 'ci') ?
       `http://${localhost()}/api/file-upload` :
       process.env.FILE_VAULT_URL
