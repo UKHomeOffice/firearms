704bb3d7aa892446f4e199af944891561056a84d
Martin Crawley <martin.crawley@digital.homeoffice.gov.uk>
2022-04-27 10:27:48 +0100

    Re-enabled new feature flag

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ module.exports = {
     secret: process.env.KEYCLOAK_SECRET
   },
   email: {
-    emailerFallback: false,
+    emailerFallback: true,
     from: process.env.FROM_ADDRESS || 'fakeemail',
     transport: process.env.EMAIL_TRANSPORT || 'ses',
     transportOptions: {
