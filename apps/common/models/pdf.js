c402645fef0914113324797616224aca08563073
SavantDhindsaHO <savantsingh.dhindsa@digital.homeoffice.gov.uk>
2023-10-22 20:35:19 +0100

    HOFF-369: Changes related to axios

--- apps/common/models/pdf.js
+++ apps/common/models/pdf.js
@@ -10,28 +10,30 @@ module.exports = class PDFModel extends Model {
   requestConfig(options) {
     const settings = super.requestConfig(options);
     settings.encoding = null;
+    console.log("Firearms settings:::",settings)
     return settings;
   }
 
   url() {
+    console.log("config.pdf.url:::",config.pdf.url)
     return config.pdf.url;
   }
 
   handleResponse(response, callback) {
-    if (_.isPlainObject(response.body)) {
-      debug('Response: %O', response.body);
+    if (_.isPlainObject(response.data)) {
+      debug('Response: %O', response.data);
     } else {
-      debug('Response: %s', response.body);
+      debug('Response: %s', response.data);
     }
-    if (isPdf(Buffer.from(response.body))) {
-      return this.parseResponse(response.statusCode, response.body, callback);
+    if (isPdf(Buffer.from(response.data))) {
+      return this.parseResponse(response.statusCode, response.data, callback);
     }
     const err = new Error();
     if (parseInt(response.statusCode, 10) === 400) {
-      err.title = response.body.code;
-      err.message = response.body.message;
+      err.title = response.data.code;
+      err.message = response.data.message;
     } else {
-      err.body = response.body;
+      err.body = response.data;
     }
     err.status = response.statusCode;
     return callback(err, null, response.statusCode);
