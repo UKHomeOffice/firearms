a6c869ff95c27ea41ef8393566e589466488df26
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2023-12-01 15:16:10 +0000

    test

--- apps/common/models/pdf.js
+++ apps/common/models/pdf.js
@@ -22,6 +22,39 @@ module.exports = class PDFModel extends Model {
     return config.pdf.url;
   }
 
+  auth() {
+    console.log("FIREARMS AUTH PDF")
+    if (!config.keycloak.token) {
+      // eslint-disable-next-line no-console
+      console.error('keycloak token url is not defined');
+      return Promise.resolve({
+        bearer: 'abc123'
+      });
+    }
+    const tokenReq = {
+      url: config.keycloak.token,
+      //headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: {
+        username: config.keycloak.username,
+        password: config.keycloak.password,
+        grant_type: 'password',
+        client_id: config.keycloak.clientId,
+        client_secret: config.keycloak.secret
+      },
+      method: 'POST'
+    };
+    console.log("%%%%%%%%%%%%%%")
+    console.log(tokenReq)
+    console.log("%%%%%%%%%%%%%%%%")
+    return new Promise((resolve, reject) => {
+      return this._request(tokenReq, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    })
+  }
 
   handleResponse(response, callback) {
     if (_.isPlainObject(response.data)) {
