947063311d715a859873d0f248ba2a5e4cd03ae8
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-07-23 13:01:58 +0100

    refactor functions in casework-submission and auth-token to async/await
    - fix error logging
    - remove duplicate headings on relevant pages
    - remove 'uat' from branch deployment message

--- apps/common/models/auth-token.js
+++ apps/common/models/auth-token.js
@@ -5,29 +5,32 @@ const Model = require('hof').model;
 const config = require('../../../config');
 
 module.exports = class AuthToken extends Model {
-  auth() {
-    if (!config.keycloak.token) {
-      // eslint-disable-next-line no-console
-      console.error('keycloak token url is not defined');
-      return Promise.resolve({
-        bearer: 'abc123'
-      });
-    }
-    const tokenReq = {
-      url: config.keycloak.token,
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: {
-        username: config.keycloak.username,
-        password: config.keycloak.password,
-        grant_type: 'password',
-        client_id: config.keycloak.clientId,
-        client_secret: config.keycloak.secret
-      },
-      method: 'POST'
-    };
-
-    return this._request(tokenReq).then(response => {
+  async auth() {
+    /* eslint-disable no-console */
+    try {
+      if (!config.keycloak.token) {
+        console.error('keycloak token url is not defined');
+        return {
+          bearer: 'abc123'
+        };
+      }
+      const tokenReq = {
+        url: config.keycloak.token,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: {
+          username: config.keycloak.username,
+          password: config.keycloak.password,
+          grant_type: 'password',
+          client_id: config.keycloak.clientId,
+          client_secret: config.keycloak.secret
+        },
+        method: 'POST'
+      };
+      const response = await this._request(tokenReq);
       return { bearer: response.data.access_token };
-    });
+    } catch (error) {
+      console.error('Error in auth method:', error);
+      throw error;
+    }
   }
 };
