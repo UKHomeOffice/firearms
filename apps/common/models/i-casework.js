c9f38f3da2fba72fb63a71637e14bd4f97e3cdae
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-09-05 11:58:29 +0100

    fix acceptance test

--- apps/common/models/i-casework.js
+++ apps/common/models/i-casework.js
@@ -36,9 +36,9 @@ module.exports = class CaseworkModel extends Model {
 
   async save() {
     try {
-      return Promise.resolve(this.prepare()).then(async data => {
+      // return Promise.resolve(this.prepare()).then(async data => {
         // const data = this.prepare();
-        // const data = await (async () => await this.prepare())();
+        const data = await (async () => await this.prepare())();
         const params = {
           url: this.url(),
           data,
@@ -58,7 +58,7 @@ module.exports = class CaseworkModel extends Model {
 
         const response = await this._request(params);
         return response;
-      });
+      // });
     } catch (err) {
       logger.error(`Error saving data: ${err.message}`);
       throw new Error(`Failed to save data: ${err.message || 'Unknown error'}`);
