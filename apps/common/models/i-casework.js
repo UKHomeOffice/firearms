f52b7cebf073c70257423cc4f5f41266205af0d6
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-09-06 10:43:56 +0100

    test error logging

--- apps/common/models/i-casework.js
+++ apps/common/models/i-casework.js
@@ -7,7 +7,7 @@ const logger = require('hof/lib/logger')({ env: config.env });
 
 module.exports = class CaseworkModel extends Model {
   url() {
-    return `${config.icasework.url}${config.icasework.createpath}`;
+    return `${config.icasework.url}${config.icasework.createpath}?db=${encodeURIComponent(config.icasework.dbName)}`;
   }
 
   sign() {
