05f60cc098626d8c6c083d88050bfe12231dceae
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2025-11-18 18:36:19 +0000

    fix url query

--- apps/supporting-documents/models/submission.js
+++ apps/supporting-documents/models/submission.js
@@ -8,7 +8,7 @@ module.exports = (data, token) => {
   const docs = data['supporting-documents'] || [];
 
   docs.forEach((doc, i) => {
-    response[`Document${i + 1}.URL`] = `${doc.url.replace('/file', '/vault')}?token=${token.bearer}`;
+    response[`Document${i + 1}.URL`] = `${doc.url.replace('/file', '/vault')}&token=${token.bearer}`;
     response[`Document${i + 1}.Name`] = doc.description;
     response[`Document${i + 1}.MimeType`] = doc.type;
     response[`Document${i + 1}.URLLoadContent`] = true;
